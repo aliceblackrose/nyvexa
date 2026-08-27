b50b5fdc1fbacb3f3296c4230a0bd623f6417e22
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-27 18:46:58 -0500

    chore: remove unused deployment plumbing

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,33 +1,13 @@
 mod ping;
 
-use std::error::Error;
-
 use gloam_commands::{Framework, Registration, commands};
-use gloamwire::model::{Interaction, InteractionMessageData, InteractionResponse};
 
 #[derive(Debug, Default)]
-pub struct CommandData;
+pub(crate) struct CommandData;
 
-pub type CommandResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
-
-pub fn framework(registration: Registration) -> gloam_commands::Result<Framework<CommandData>> {
+pub(crate) fn framework(registration: Registration) -> gloam_commands::Result<Framework<CommandData>> {
     Framework::builder(CommandData)
         .commands(commands![ping::ping])
         .registration(registration)
         .build()
-}
-
-pub fn response(interaction: &Interaction) -> CommandResult<Option<InteractionResponse>> {
-    let Some(command) = interaction.application_command_data()? else {
-        return Ok(None);
-    };
-
-    let content = match command.name.as_str() {
-        ping::NAME => ping::response(),
-        _ => return Ok(None),
-    };
-
-    Ok(Some(InteractionResponse::message(
-        InteractionMessageData::content(content),
-    )))
 }
--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -2,14 +2,7 @@ use gloam_commands::prelude::*;
 
 use super::CommandData;
 
-pub const NAME: &str = "ping";
-
-#[command(name = "ping", description = "Check whether Nyvexa is online")]
+#[command(description = "Check whether Nyvexa is online")]
 pub(crate) async fn ping(ctx: Context<CommandData>) -> Result<()> {
-    ctx.reply(response()).await?;
-    Ok(())
-}
-
-pub const fn response() -> &'static str {
-    "Pong!"
+    ctx.reply("Pong!").await
 }
